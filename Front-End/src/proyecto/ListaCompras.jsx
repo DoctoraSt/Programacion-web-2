ec9d9056ed72b3503d198a59c9b89eec012051b3
DoctoraSt <leal.lf._@hotmail.com>
2024-10-21 20:01:09 -0600

    ajustes

--- Front-End/src/proyecto/ListaCompras.jsx
+++ Front-End/src/proyecto/ListaCompras.jsx
@@ -1,48 +1,54 @@
 import React from "react";
-import { Navigate,Link} from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
 import '../CSS/login.css';
 import icono from '../CSS/images/list.png';
 import '../CSS/bootstrapCSS/bootstrap.css';
 
-function ListaCompras(){
+function ListaCompras() {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-            <img src={icono} alt="icono"  height={50} width={50}/>
-            <a class="navbar-brand" href="#">Shopping List</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-        <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
-            <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Mi perfil
-                    </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Información personal</a></li>
-                <li><a class="dropdown-item" href="#">Tarjetas</a></li>
-                <li><a class="dropdown-item" href="#">Direcciones</a></li>
-                <li><a class="dropdown-item" href="#">Comunicación</a></li>
-            </ul>
-        </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Lo más vendido</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Promociones</a>
-                </li>
-        </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-secondary" type="submit">Search</button>
-            </form>
-        </div>
-        </div>
-    </nav>
+        <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <img src={icono} alt="icono" height={50} width={50} />
+                <a className="navbar-brand" href="#">Shopping List</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Mi perfil
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Información personal</a></li>
+                                <li><a className="dropdown-item" href="#">Tarjetas</a></li>
+                                <li><a className="dropdown-item" href="#">Direcciones</a></li>
+                                <li><a className="dropdown-item" href="#">Comunicación</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Lo más vendido</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Promociones</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-secondary" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+        <section>
+            {/* Agrega contenido aquí */}
+        </section>
+        </>
     );
 }
 
